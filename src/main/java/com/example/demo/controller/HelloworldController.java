de7cf25256c57c2373b90119dff62ca7988a3ea1
t-ura.tjs <t-ura@techno-jouhou.co.jp>
2022-04-26 14:20:36 +0900

    【2022/04/26】新規作成

---
+++ src/main/java/com/example/demo/controller/HelloworldController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloworldController {
+	@RequestMapping("/")
+	private String index() {
+		return "hello";
+	}
+
+	@GetMapping("/get-message")
+//  private String getMessage(@RequestParam("message") String message, Model model) {
+	private String getMessage(@RequestParam(required = false) String message, Model model) {
+		model.addAttribute("message", message);
+		return "get_message";
+	}
+}
